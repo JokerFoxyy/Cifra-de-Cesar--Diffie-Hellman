cb1e289733f39de6b28e8485b9b0677f361fb0ef
VICTOR ASSAGRA <082190021@faculdade.cefsa.edu.br>
2024-02-22 22:42:52 -0300

    .

--- server.py
+++ server.py
@@ -1,11 +1,26 @@
 from socket import *
-serverPort = 1200
+serverPort = 1300
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(("",serverPort))
 serverSocket.listen(5) # o argumento “listen” diz à biblioteca de soquetes que queremos enfileirar no máximo 5 requisições de conexão (normalmente o máximo) antes de recusar começar a recusar conexões externas. Caso o resto do código esteja escrito corretamente, isso deverá ser o suficiente.
 print ("TCP Server\n")
 
 connectionSocket, addr = serverSocket.accept()
+
+print ("Server waiting key\n")
+received_message = connectionSocket.recv(1024)
+G, N, R1 = map(int, received_message.decode("utf-8").split(","))
+Y = 123
+print("Y: ", Y)
+
+R2 = (G ** Y) % N
+print("R2: ", R2)
+
+K = (R1 ** Y) % N
+print("K: ", K)
+
+connectionSocket.send(bytes(str(R2), "utf-8"))
+
 sentence = connectionSocket.recv(65000)
 
 print(f'Recebido Criptografado: {str(sentence, "utf-8")}')
@@ -14,7 +29,7 @@ decrypted = bytes([byte - 3 for byte in sentence])
 
 received = str(decrypted, "utf-8")
 
-print ("Recebido decriptografado:", received)
+print ("Decriptografado:", received)
 
 capitalizedSentence = received.upper()
 
